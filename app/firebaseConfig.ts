7d41da3d4f31a2113333e8cac35b374fd5f0bdfa
Cesar [Web&Blockchain] <75784005+cesar4design@users.noreply.github.com>
2024-10-11 19:44:39 +0200

    Update firebaseConfig.ts

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Init Firebase
+// Ini Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
